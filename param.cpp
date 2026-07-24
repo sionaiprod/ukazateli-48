81a070191edd4c286c22e3c6ba18d71f2c8791ef
sionaiprod <kalashnikovsergey0303@gmail.com>
2026-07-24 21:44:21 +0300

    #49

--- param.cpp
+++ param.cpp
@@ -4,33 +4,55 @@ using namespace std;
 // передача аргументов по указателю
 // как вернуть 2 или более значения из функции
 
-void Foo(int a){
-    a++;
-}
+// void Foo(int a){
+//     a++;
+// }
 
-void Foo2(int *pb){
+// void Foo2(int *pb){
+//     (*pb)++;
+// }
+
+void Foo3(int *pa, int *pb, int *pc){
+    (*pa)= 555;
     (*pb)++;
+    (*pc) = -20;
 }
 
 int main() {
 
     // изменений не произойдет
-    int a = 0;
-    cout << "a = " << a << endl;
+    // int a = 0;
+    // cout << "a = " << a << endl;
 
-    Foo(a);
+    // Foo(a);
 
-    cout << "a = " << a << endl;
-    cout << "-------------------"<< endl;
+    // cout << "a = " << a << endl;
+    // cout << "-------------------"<< endl;
 
     // изменения будут
+    // int b = 0;
+    // cout << "b = " << b << endl;
+
+    // Foo2(&b);
+
+    // cout << "b = " << b << endl;
+    // cout << "-------------------"<< endl;
+
+    int a = 0;
     int b = 0;
-    cout << "b = " << b << endl;
+    int c = 1;
 
-    Foo2(&b);
-
+    cout << "a = " << a << endl;
     cout << "b = " << b << endl;
-    cout << "-------------------"<< endl;
+    cout << "c = " << c << endl;
+
+    cout << "foo()" << endl;
+    Foo3(&a, &b, &c);
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+
 
 
     return 0;
