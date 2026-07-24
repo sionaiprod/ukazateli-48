44490d9175ce7b9689b2fc2ed350130746b0764d
sionaiprod <kalashnikovsergey0303@gmail.com>
2026-07-24 21:57:46 +0300

    DZ (ukazateli)

--- param.cpp
+++ param.cpp
@@ -12,10 +12,18 @@ using namespace std;
 //     (*pb)++;
 // }
 
-void Foo3(int *pa, int *pb, int *pc){
-    (*pa)= 555;
-    (*pb)++;
-    (*pc) = -20;
+// void Foo3(int *pa, int *pb, int *pc){
+//     (*pa)= 555;
+//     (*pb)++;
+//     (*pc) = -20;
+// }
+
+int da;
+
+void swap(int *pa, int *pb){
+    da = *pa;
+    *pa = *pb;
+    *pb = da;
 }
 
 int main() {
@@ -38,22 +46,40 @@ int main() {
     // cout << "b = " << b << endl;
     // cout << "-------------------"<< endl;
 
-    int a = 0;
-    int b = 0;
-    int c = 1;
+    // int a = 0;
+    // int b = 0;
+    // int c = 1;
+
+    // cout << "a = " << a << endl;
+    // cout << "b = " << b << endl;
+    // cout << "c = " << c << endl;
+
+    // cout << "foo()" << endl;
+    // Foo3(&a, &b, &c);
+
+    // cout << "a = " << a << endl;
+    // cout << "b = " << b << endl;
+    // cout << "c = " << c << endl;
+
+    // cout << "-------------------"<< endl;
+
+    // Домашнее задание 
+
+    int a = 3;
+    int b = 5; 
+
+
 
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
-    cout << "c = " << c << endl;
+    cout << "da = " << da << endl;
 
     cout << "foo()" << endl;
-    Foo3(&a, &b, &c);
+    swap(&a, &b);
 
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
-    cout << "c = " << c << endl;
-
-
+    cout << "da = " << da << endl;
 
     return 0;
 }
